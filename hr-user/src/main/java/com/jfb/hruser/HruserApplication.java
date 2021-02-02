fbefb4db2006f2cb5a732a9ad017269484d00b5c
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-02 09:24:27 -0300

    Project hr-user created.

---
+++ hr-user/src/main/java/com/jfb/hruser/HruserApplication.java
@@ -0,0 +1,13 @@
+package com.jfb.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HruserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HruserApplication.class, args);
+	}
+
+}
