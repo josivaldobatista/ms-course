f5bf2eaa85f6f2fc60ddb1c2d315a5afff1c01ca
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-02 11:29:41 -0300

    Database seeding.

--- hr-user/src/main/java/com/jfb/hruser/HruserApplication.java
+++ hr-user/src/main/java/com/jfb/hruser/HruserApplication.java
@@ -1,15 +1,22 @@
 package com.jfb.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HruserApplication {
+public class HruserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HruserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// System.out.println("BCrypt: " + encoder.encode("123456"));
+
 	}
 
 }
--- hr-user/src/main/java/com/jfb/hruser/entities/User.java
+++ hr-user/src/main/java/com/jfb/hruser/entities/User.java
@@ -24,21 +24,20 @@ public class User implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  private String name;
   private String email;
   private String password;
 
   @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(
-    name = "tb_user_role",
-    joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
+  @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles = new HashSet<>();
 
   public User() {
   }
 
-  public User(Long id, String email, String password) {
+  public User(Long id, String name, String email, String password) {
     this.id = id;
+    this.name = name;
     this.email = email;
     this.password = password;
   }
@@ -49,6 +48,14 @@ public class User implements Serializable {
 
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   public String getEmail() {
